f0eb9b44de5523d80dd79aa9e118d426a0d0b741
Yannik F <73497566+reversum@users.noreply.github.com>
2025-07-16 23:56:13 +0200

    Projektdateien hinzufügen.

---
+++ WeaponDamageMultiplier/Config.cs
@@ -0,0 +1,49 @@
+using Exiled.API.Interfaces;
+using MapGeneration.Distributors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YamlDotNet.Core;
+
+namespace HitboxWeaponDamageMultiplier
+{
+	public sealed class Config : IConfig
+	{
+		[Description("Whether the plugin is enabled.")]
+		public bool IsEnabled { get; set; } = true;
+
+		[Description("Enable debug logs.")]
+		public bool Debug { get; set; } = false;
+
+		[Description("Default multipliers used if no weapon-specific multipliers are defined.")]
+		public Dictionary<string, float> DefaultMultipliers { get; set; } = new Dictionary<string, float>()
+		{
+			{ "headshot", 2.0f },
+			{ "body", 1.0f },
+			{ "limb", 0.8f },
+		};
+
+		[Description("Weapon-specific damage multipliers per hitbox.")]
+		public Dictionary<string, Dictionary<string, float>> WeaponMultipliers { get; set; } =
+			new Dictionary<string, Dictionary<string, float>>()
+		{
+			{ "GunCOM15", new Dictionary<string, float>()
+				{
+					{ "headshot", 3.0f },
+					{ "body", 1.0f },
+					{ "limb", 0.8f },
+				}
+			},
+			{ "GunE11SR", new Dictionary<string, float>()
+				{
+					{ "headshot", 3.0f },
+					{ "body", 1.0f },
+					{ "limb", 0.8f },
+				}
+			}
+		};
+	}
+}
---
+++ WeaponDamageMultiplier/Main.cs
@@ -0,0 +1,74 @@
+using Exiled.API.Enums;
+using Exiled.API.Features;
+using Exiled.API.Features.Items;
+using Exiled.Events.EventArgs.Player;
+using PlayerRoles;
+using PlayerStatsSystem;
+using System;
+using System.Collections.Generic;
+
+namespace HitboxWeaponDamageMultiplier
+{
+	public class Main : Plugin<Config>
+	{
+		public override string Name => "HitboxWeaponDamageMultiplier";
+		public override string Author => "YannikAufDie1";
+		public override PluginPriority Priority { get; } = PluginPriority.Last;
+		public override void OnEnabled()
+		{
+			Exiled.Events.Handlers.Player.Hurting += OnHurting;
+			base.OnEnabled();
+		}
+
+		public override void OnDisabled()
+		{
+			Exiled.Events.Handlers.Player.Hurting -= OnHurting;
+			base.OnDisabled();
+		}
+
+		private void OnHurting(HurtingEventArgs ev)
+		{
+			if (!ev.IsAllowed || ev.Attacker == null) return;
+
+			string weaponName = GetWeaponName(ev);
+
+			if (Config.Debug)
+			{
+				Log.Debug($"[DEBUG] Weaponname: {weaponName}");
+			}
+
+			StandardDamageHandler standardDamageHandler = (StandardDamageHandler)ev.DamageHandler;
+			string hitbox = standardDamageHandler.Hitbox.ToString().ToLower();
+
+			Dictionary<string, float> multipliers = Config.DefaultMultipliers;
+
+			if (Config.WeaponMultipliers.ContainsKey(weaponName))
+			{
+				multipliers = Config.WeaponMultipliers[weaponName];
+			}
+
+			float multiplier = multipliers.ContainsKey(hitbox) ? multipliers[hitbox] : 1.0f;
+
+			if (multiplier != 1.0f)
+			{
+				float oldDamage = ev.Amount;
+				ev.Amount *= multiplier;
+
+				if (Config.Debug)
+				{
+					Log.Debug($"[DEBUG] {ev.Attacker.Nickname} → {ev.Player.Nickname} | Weapon: {weaponName}, Hitbox: {hitbox}, Multiplier: x{multiplier}, Damage: {oldDamage} → {ev.Amount}");
+				}
+			}
+		}
+
+		private string GetWeaponName(HurtingEventArgs ev)
+		{
+			if (ev.Attacker.CurrentItem is Firearm firearm)
+				return firearm.Type.ToString();
+			else if (ev.Attacker.Role.Team == Team.SCPs)
+				return ev.Attacker.Role.Type.ToString();
+			else
+				return "Unknown";
+		}
+	}
+}
